e065c624621121a2d9c4ac8e3e62e9cd84511ae9
rebfarish <rebfarish@gmail.com>
2018-11-01 10:04:41 -0600

    Defined service and model.

--- app/src/main/java/edu/cnm/deepdive/nasaapod/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/controller/MainActivity.java
@@ -1,15 +1,24 @@
-package edu.cnm.deepdive.nasaapod;
+package edu.cnm.deepdive.nasaapod.controller;
 
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import edu.cnm.deepdive.nasaapod.BuildConfig;
+import edu.cnm.deepdive.nasaapod.R;
+import edu.cnm.deepdive.nasaapod.model.Apod;
+import edu.cnm.deepdive.nasaapod.service.ApodService;
 import java.util.Calendar;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,21 +30,24 @@ public class MainActivity extends AppCompatActivity {
   private WebView webView;
   private String apiKey;
   private ProgressBar progressSpinner;
-  private FloatingActionButton jumpdate;
+  private FloatingActionButton jumpDate;
   private Calendar calender;
+  private ApodService service;
+  private Apod apod;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     setupWebView();
-    //TODO setup service
-    //TODO setup UI
+    setupService();
+    setupUI();
     //TODO setup default
   }
-  private void setupWebView(){
+
+  private void setupWebView() {
     webView = findViewById(R.id.web_view);
-    webView.setWebViewClient(new WebViewClient(){
+    webView.setWebViewClient(new WebViewClient() {
       @Override
       public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
         return false;
@@ -54,5 +66,31 @@ public class MainActivity extends AppCompatActivity {
     settings.setDisplayZoomControls(false);
     settings.setUseWideViewPort(true);
     settings.setLoadWithOverviewMode(true);
+  }
+
+  private void setupUI() {
+    progressSpinner = findViewById(R.id.progress_spinner);
+    progressSpinner.setVisibility(View.GONE);
+    jumpDate = findViewById(R.id.jump_date);
+    jumpDate.setOnClickListener(new OnClickListener() {
+      //TODO use lambda
+      @Override
+      public void onClick(View v) {
+        //TODO Display date picker
+      }
+    });
+  }
+
+  private void setupService(){
+    Gson gson = new GsonBuilder()
+        .excludeFieldsWithoutExposeAnnotation()
+        .setDateFormat(DATE_FORMAT)
+        .create();
+    Retrofit retrofit = new Retrofit.Builder()
+        .baseUrl(getString(R.string.base_url))
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build();
+    service = retrofit.create(ApodService.class);
+    apiKey = BuildConfig.API_KEY;
   }
 }
